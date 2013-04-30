96db0e18440762a8f3cf152aa60be9e886ad49a2
Nuke <flow.energy@gmail.com>
2013-04-30 17:26:19 +0400

    commit

--- compare.rb
+++ compare.rb
@@ -1,53 +1,71 @@
-CHUNK_SIZE = 2**16
-MULTIPLIER = 8
-require 'benchmark'
-def compare(old, recent)
-  [old,recent].each { |file|
-    abort("'#{file}' does not exist or not readable") unless File.exist?(file) && File.readable?(file)
+CHUNK_SIZE = 2**22
+MULTIPLIER = 1
+## Функция выделения изменившихся путей средствами Ruby
+# Params:
+# +old+:: Файл со списком путей предыдущей версии
+# +recent+:: Файл со списком путей новой версии
+def compare(old = nil, recent = nil)
+  [old, recent].each { |file|
+    # Проверка существования каждого из файлов и проверка возможности чтения файлов
+    # В случае несуществующего пути или недоступного файла выводит false
+    return false unless file && File.exist?(file) && File.readable?(file)
   }
-
-  removed = []
-  added = []
-
+  added, removed = [], []
+  # Процедура вычленения изменившихся файлов, принимает два параметра - объекты типа Enumerator
   extractor = Proc.new do |fs1, fs2|
-    old_file_chunk = fs1.next
-    new_file_chunk = fs2.next
-
-    #old_file_chunk.map! { |e| e.strip }
-    #new_file_chunk.map! { |e| e.strip }
-    added_files = new_file_chunk - old_file_chunk
-    removed_files = old_file_chunk - new_file_chunk
-    # added_files = [new_file_chunk, old_file_chunk].reduce(:-)
-    # removed_files = [old_file_chunk, new_file_chunk].reduce(:-)
-
+    # Вызываются следующие блоки данных из обоих файлов
+    b_o = fs1.next
+    b_n = fs2.next
+    # Проводится вычленение нужных элементов путём пересечения массивов в одном и в другом направлении
+    added_files = b_n - b_o
+    removed_files = b_o - b_n
+    # Процедура отдаёт массив с двумя субмассивами добавленных и удалённых файлов
     [added_files, removed_files]
-
   end
+  # Открываются файловые потоки по обоим файлам.
+  # Потоки бьются на куски размером +CHUNK_SIZE+ байт.
+  old_file_stream = File.open(old, "r").each_slice(CHUNK_SIZE)
+  new_file_stream = File.open(recent, "r").each_slice(CHUNK_SIZE)
 
-  io1 = File.open(old, "r").each_slice(CHUNK_SIZE)
-  io2 = File.open(recent, "r").each_slice(CHUNK_SIZE)
-
-  #workers = File.size(old).fdiv(CHUNK_SIZE*MULTIPLIER).ceil
-
-  loop do
+  # Высчитывается количество запусков процедуры вычленения исходя из количества блоков размером +CHUNK_SIZE+ в исходном файле.
+  workers = File.size(old).fdiv(CHUNK_SIZE*MULTIPLIER).ceil
+  # Запускается Fiber с процедурой вычленения данных.
+  workers.times {
     Fiber.new {
-      puts "started new extract"
-      old_file_chunk = io1.next
-      new_file_chunk = io2.next
-      added.push([new_file_chunk - old_file_chunk]).flatten!
-      removed.push([old_file_chunk - new_file_chunk]).flatten!
-      puts "finished"
+      begin
+        a,r = extractor.call(old_file_stream, new_file_stream)
+        added.push(a).flatten!
+        removed.push(r).flatten!
+      rescue StopIteration
+      end
     }.resume
-  end
-
+  }
+  # Выводом является массив со списками значений добавленных и удалённых файлов
   [added, removed]
 end
 
-Benchmark.bm do |test|
-  test.report { a,r = compare("filepaths.txt", "filepaths2.txt")
-    puts "added:"
-    puts a
-    puts "removed:"
-    puts r
+## Функция выделения изменившихся путей через средства *nix
+# Params:
+# +old+:: Файл со списком путей предыдущей версии
+# +recent+:: Файл со списком путей новой версии
+def compare_native(old = nil, recent = nil)
+  # Проверка существования каждого из файлов и проверка возможности чтения файлов
+  # В случае несуществующего пути или недоступного файла выводит false
+  [old,recent].each { |file|
+    return false unless file && File.exist?(file) && File.readable?(file)
   }
+  added, removed = [], []
+  # Результат вызова diff на оба файла выводится в массив Array
+  diff = %x(diff -wBE --speed-large-files #{old} #{recent}).split("\n")
+  # Каждый из элементов проходит проверки
+  diff.each { |e|
+    # Отсекается ненужный вывод от diff, оставляя только изменения и флаг привязки к файлу
+    next unless /^[<>]\s.*$/.match(e)
+    # Выводится направление изменений и изменившийся элемент
+    direction, element = e.split
+    # Элементы с флагом "<"" добавляются в список удалённых файлов, элементы с флагом ">" в список добавленных
+    direction == "<" and removed.push(element) or added.push(element)
+  }
+  # Выводом является массив со списками значений добавленных и удалённых файлов
+  [added, removed]
 end
---
+++ spec/comparator_spec.rb
@@ -0,0 +1,47 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+require File.dirname(__FILE__) + '/../compare.rb'
+
+describe "Comparator" do
+  before :all do
+    stub = ContentGenerator.new
+    @changed = stub.changed
+    @unchanged = stub.unchanged
+  end
+  describe "Native comparator" do
+    it "should return correct added/removed" do
+      added, removed = compare_native(@changed[:old_file], @changed[:new_file])
+      expect(added.size).to be == @changed[:added]
+      expect(removed.size).to be == @changed[:removed]
+    end
+
+    it "should return false on wrong input" do
+      result = compare_native("/random/wrong")
+      expect(compare_native).to be_false
+    end
+
+    it "should return 0 when files are identical" do
+      added, removed = compare_native(@unchanged[:old_file], @unchanged[:new_file])
+      expect(added.size).to be == @unchanged[:added]
+      expect(removed.size).to be == @unchanged[:removed]
+    end
+  end
+
+  describe "Ruby comparator" do
+    it "should return correct added/removed" do
+      added, removed = compare(@changed[:old_file], @changed[:new_file])
+      expect(added.size).to be == @changed[:added]
+      expect(removed.size).to be == @changed[:removed]
+    end
+
+    it "should return false on wrong input" do
+      result = compare("/random/wrong")
+      expect(compare_native).to be_false
+    end
+
+    it "should return 0 when files are identical" do
+      added, removed = compare(@unchanged[:old_file], @unchanged[:new_file])
+      expect(added.size).to be == @unchanged[:added]
+      expect(removed.size).to be == @unchanged[:removed]
+    end
+  end
+end
