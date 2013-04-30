0d113efdd325cf0885dd84e6c06a92c3582f4590
Nuke <flow.energy@gmail.com>
2013-04-30 15:19:12 +0400

    latest

--- compare.rb
+++ compare.rb
@@ -1,7 +1,8 @@
-def compare(f1, f2)
-  CHUNK_SIZE = 2**27
-  MULTIPLIER = 1
-  [f1,f2].each { |file|
+CHUNK_SIZE = 2**16
+MULTIPLIER = 8
+require 'benchmark'
+def compare(old, recent)
+  [old,recent].each { |file|
     abort("'#{file}' does not exist or not readable") unless File.exist?(file) && File.readable?(file)
   }
 
@@ -9,77 +10,44 @@ def compare(f1, f2)
   added = []
 
   extractor = Proc.new do |fs1, fs2|
-    b_o = fs1.next
-    b_n = fs2.next
+    old_file_chunk = fs1.next
+    new_file_chunk = fs2.next
 
-    b_o.map { |e| e.strip }
-    b_n.map { |e| e.strip }
-
-    added_files = b_n - b_o
-    removed_files = b_o - b_n
+    #old_file_chunk.map! { |e| e.strip }
+    #new_file_chunk.map! { |e| e.strip }
+    added_files = new_file_chunk - old_file_chunk
+    removed_files = old_file_chunk - new_file_chunk
+    # added_files = [new_file_chunk, old_file_chunk].reduce(:-)
+    # removed_files = [old_file_chunk, new_file_chunk].reduce(:-)
 
     [added_files, removed_files]
 
   end
 
-  io1 = File.open(f1, "r").each_slice(CHUNK_SIZE)
-  io2 = File.open(f2, "r").each_slice(CHUNK_SIZE)
+  io1 = File.open(old, "r").each_slice(CHUNK_SIZE)
+  io2 = File.open(recent, "r").each_slice(CHUNK_SIZE)
 
-  workers = File.size(f1).fdiv(CHUNK_SIZE*MULTIPLIER).ceil
+  #workers = File.size(old).fdiv(CHUNK_SIZE*MULTIPLIER).ceil
 
-  workers.times {
+  loop do
     Fiber.new {
-      begin
-        a,r = extractor.call(io1, io2)
-        added.push(a).flatten!
-        removed.push(r).flatten!
-      rescue StopIteration
-      end
+      puts "started new extract"
+      old_file_chunk = io1.next
+      new_file_chunk = io2.next
+      added.push([new_file_chunk - old_file_chunk]).flatten!
+      removed.push([old_file_chunk - new_file_chunk]).flatten!
+      puts "finished"
     }.resume
-  }
-  # File.open("added_files.txt", "w+") { |f| f.puts(added) } if added.any?
-  # File.open("removed_files.txt", "w+") { |f| f.puts(removed) } if removed.any?
-  # (puts "Added files"; added.each { |p| puts p }) if added.any?
-  # (puts "Removed files"; removed.each { |p| puts p }) if removed.any?
+  end
+
+  [added, removed]
 end
 
-def compare2(f1, f2)
-  [f1,f2].each { |file|
-    abort("'#{file}' does not exist or not readable") unless File.exist?(file) && File.readable?(file)
+Benchmark.bm do |test|
+  test.report { a,r = compare("filepaths.txt", "filepaths2.txt")
+    puts "added:"
+    puts a
+    puts "removed:"
+    puts r
   }
-
-  removed = []
-  added = []
-
-  extractor = Proc.new do |fs1, fs2|
-    b_o = fs1.next
-    b_n = fs2.next
-
-    b_o.map { |e| e.strip }
-    b_n.map { |e| e.strip }
-
-    added_files = b_n - b_o
-    removed_files = b_o - b_n
-
-    [added_files, removed_files]
-
-  end
-
-  file1 = File.open(f1, "r")
-  io1 = file1.each_slice(CHUNK_SIZE)
-  file2 = File.open(f2, "r")
-  io2 = file2.each_slice(CHUNK_SIZE)
-
-  until io1.eof? && io2.eof? do
-    begin
-      a,r = extractor.call(io1, io2)
-      added.push(a).flatten!
-      removed.push(r).flatten!
-    rescue StopIteration
-    end
-  end
-  # File.open("added_files.txt", "w+") { |f| f.puts(added) } if added.any?
-  # File.open("removed_files.txt", "w+") { |f| f.puts(removed) } if removed.any?
-  # (puts "Added files"; added.each { |p| puts p }) if added.any?
-  # (puts "Removed files"; removed.each { |p| puts p }) if removed.any?
 end
